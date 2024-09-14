6d82773580de609ae50c27f69885b3c209dc0e38
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-09-14 16:37:32 +0300

    Initial Commit

---
+++ CustomNavigationStack/CustomNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationStackApp.swift
+//  CustomNavigationStack
+//
+//  Created by Volodymyr Myroniuk on 14.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationStackApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
