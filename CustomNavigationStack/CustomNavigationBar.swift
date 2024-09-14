b0e97f937eb733a30f94bb8926bb392e071f97cc
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-09-14 17:21:50 +0300

    Add a custom navigation bar

---
+++ CustomNavigationStack/CustomNavigationBar.swift
@@ -0,0 +1,61 @@
+//
+//  CustomNavigationBar.swift
+//  CustomNavigationStack
+//
+//  Created by Volodymyr Myroniuk on 14.09.2024.
+//
+
+import SwiftUI
+
+struct CustomNavigationBar: View {
+  @State private var showBackButton: Bool = true
+  @State private var title: String = "Title"
+  @State private var subtitle: String? = "Subtitle"
+  
+  var body: some View {
+    HStack {
+      if showBackButton {
+        backButton
+      }
+      Spacer()
+      titleSection
+      Spacer()
+      if showBackButton {
+        backButton
+          .opacity(0)
+      }
+    }
+    .padding()
+    .tint(.white)
+    .foregroundStyle(.white)
+    .font(.headline)
+    .background(Color.blue)
+  }
+}
+
+private extension CustomNavigationBar {
+  private var backButton: some View {
+    Button {
+    } label: {
+      Image(systemName: "chevron.left")
+    }
+  }
+  
+  private var titleSection: some View {
+    VStack {
+      Text(title)
+        .font(.title)
+        .fontWeight(.semibold)
+      if let subtitle {
+        Text(subtitle)
+      }
+    }
+  }
+}
+
+#Preview {
+  VStack {
+    CustomNavigationBar()
+    Spacer()
+  }
+}
